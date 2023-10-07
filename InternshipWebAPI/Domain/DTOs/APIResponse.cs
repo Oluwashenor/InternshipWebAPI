5ac4f3cba78ace221086b3eff98ec81468506d67
Adesina <adeshiname@gmail.com>
2023-10-08 00:42:26 +0100

    Response Utility

---
+++ InternshipWebAPI/Domain/DTOs/APIResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace InternshipWebAPI.Domain.DTOs
+{
+    public class APIResponse<T>
+    {
+        public T Data { get; set; }
+        public bool Status { get; set; }
+        public string? Message { get; set; }
+    }
+}
---
+++ InternshipWebAPI/Utilities/ResponseService.cs
@@ -0,0 +1,32 @@
+﻿using InternshipWebAPI.Domain.DTOs;
+
+namespace InternshipWebAPI.Utilities
+{
+    public class ResponseService : IResponseService
+    {
+        public APIResponse<T> SuccessResponse<T>(T data, string? message = null)
+        {
+            return new APIResponse<T>
+            {
+                Status = true,
+                Data = data,
+                Message = message ?? "Successful Operation"
+            };
+        }
+
+        public APIResponse<T> ErrorResponse<T>(string? message = null)
+        {
+            return new APIResponse<T>
+            {
+                Status = false,
+                Message = message ?? "Something went wrong"
+            };
+        }
+    }
+
+    public interface IResponseService
+    {
+        APIResponse<T> ErrorResponse<T>(string? message = null);
+        APIResponse<T> SuccessResponse<T>(T data, string? message = null);
+    }
+}
