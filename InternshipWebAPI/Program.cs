871fc5e55c454e3a50cf224748dbecc526920c19
Adesina <adeshiname@gmail.com>
2023-10-07 23:58:50 +0100

    DB and Packages

--- InternshipWebAPI/Program.cs
+++ InternshipWebAPI/Program.cs
@@ -1,11 +1,22 @@
+using InternshipWebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var config = builder.Configuration;
+var connectionString = config.GetConnectionString("LocalCon");
+var cosmosDB = config.GetConnectionString("DatabaseName");
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseCosmos(connectionString, cosmosDB);
+});
 
 var app = builder.Build();
 
